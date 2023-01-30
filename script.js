cc397c528523c41874bb39d619f63d286c6aff7f
Tom <tomking151183@gmail.com>
2023-01-30 11:10:48 +0000

    api for city search

--- script.js
+++ script.js
@@ -39,3 +39,67 @@ $(document).ready(function () {
     }
 
     loadRecentCityList();
+
+    // event handler for search button
+    $('#submit').on('click', function (event) => {
+        event.preventDefault();
+        getCity();
+        search();
+        $('#city-input').val('');
+        listCityList();
+    });
+
+    // function save searched cityList to local storage
+    function saveToLocalStorage() {
+        localStorage.setItem('mostRecent', city);
+        cityList.push(city);
+        localStorage.setItem('cityList', JSON.stringify(cityList));
+    }
+
+    // function that gets city from input field
+    function getCity() {
+        city = $('#city-input').val();
+        if (city && cityList.includes(city) === false) {
+            saveToLocalStorage();
+            return city;
+        } else if (!city) {
+            alert('Please enter a city');
+        }
+    }
+
+    // search API for city
+    function search() {
+        let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7fb4c3a331d8f5c01c349dc0e1c06add';
+        let coords = [];
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            coords.push(response.coord.lat);
+            coords.push(response.coord.lon);
+            let cityName = response.name;
+            let cityCond = response.weather[0].description;
+            let cityTemp = response.main.temp;
+            let cityHum = response.main.humidity;
+            let cityWind = response.wind.speed;
+            let icon = response.weather[0].icon;
+            $('#icon').html('<img src='http://openweathermap.org/img/wn/${icon}.png'>');
+            $(#city-name).html(cityName + " " + "(" + currentDayTime + ")");
+            $("#city-cond").text("Current Conditions: " + cityCond);
+      $("#temp").text("Current Temp (F): " + cityTemp.toFixed(1));
+      $("#humidity").text("Humidity: " + cityHum + "%");
+      $("#wind-speed").text("Wind Speed: " + cityWind + "mph");
+      $("#date1").text(day1);
+      $("#date2").text(day2);
+      $("#date3").text(day3);
+      $("#date4").text(day4);
+      $("#date5").text(day5);
+
+      getUV(response.coord.lat, response.coord.lon);
+    }).fail(function (){
+      alert("Data not available")
+    });
+    }
+
+    
