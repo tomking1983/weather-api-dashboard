5942b6e3bc4ef12bd19d5988b8895c84a3edda2a
Tom <tomking151183@gmail.com>
2023-02-01 10:48:59 +0000

    final tweaks

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-'use strict';
-
 $(document).ready(function () {
-
   //Pulls the current date
   let dateTime = moment().format("DD/MM/YYYY, h:mm:ss a");
   // update time every second
@@ -9,9 +6,7 @@ $(document).ready(function () {
     dateTime = moment().format("DD/MM/YYYY, h:mm:ss a");
     $("#date-time").text(dateTime);
   }, 1000);
-  
 
-  
   //adds days to moment for 5 day forecast
   let day1 = moment().add(1, "days").format("DD/MM/YYYY");
   let day2 = moment().add(2, "days").format("DD/MM/YYYY");
@@ -19,13 +14,11 @@ $(document).ready(function () {
   let day4 = moment().add(4, "days").format("DD/MM/YYYY");
   let day5 = moment().add(5, "days").format("DD/MM/YYYY");
 
-
-
- //global variables
+  //global variables
   let city;
   let cities;
 
- //function to load most recently searched city from local storage if nothing in local storage then default to Oswestry
+  //function to load most recently searched city from local storage if nothing in local storage then default to Oswestry
   function loadMostRecent() {
     let recentCity = localStorage.getItem("mostRecent");
     if (recentCity) {
@@ -37,9 +30,9 @@ $(document).ready(function () {
     }
   }
 
-  loadMostRecent()
+  loadMostRecent();
 
-//function to load recently searched cities from local storage
+  //function to load recently searched cities from local storage
   function loadRecentCities() {
     let recentCities = JSON.parse(localStorage.getItem("cities"));
 
@@ -50,7 +43,7 @@ $(document).ready(function () {
     }
   }
 
-  loadRecentCities()
+  loadRecentCities();
 
   //event handler for search city button
   $("#submit").on("click", (e) => {
@@ -60,7 +53,6 @@ $(document).ready(function () {
     $("#city-input").val("");
     listCities();
   });
-
 
   //function to save searched cities to local storage
   function saveToLocalStorage() {
@@ -76,60 +68,63 @@ $(document).ready(function () {
       saveToLocalStorage();
       return city;
     } else if (!city) {
-      alert("Please enter a valid city");
+      alert("Please enter a valid city/town");
     }
   }
 
-
   // searches the API for the chosen city
   function search() {
-    
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=9f052728837492dee784a6ea4d36509f";
+    let queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=9f052728837492dee784a6ea4d36509f";
     let coords = [];
 
     $.ajax({
       url: queryURL,
       method: "GET",
-    }).then(function (response) {
-      
-      coords.push(response.coord.lat);
-      coords.push(response.coord.lon);
-      let cityName = response.name;
-      let cityCond = response.weather[0].description.toUpperCase();
-      let cityTemp = response.main.temp;
-      let cityHum = response.main.humidity;
-      let cityWind = response.wind.speed;
-      let icon = response.weather[0].icon;
-      $("#icon").html(
-        `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
-      );
-      $("#city-name").html(cityName);
-      $("#city-cond").text("Current Conditions: " + cityCond);
-      $("#temp").text("Current Temp (F): " + cityTemp.toFixed(1));
-      $("#humidity").text("Humidity: " + cityHum + "%");
-      $("#wind-speed").text("Wind Speed: " + cityWind + "mph");
-      $("#date1").text(day1);
-      $("#date2").text(day2);
-      $("#date3").text(day3);
-      $("#date4").text(day4);
-      $("#date5").text(day5);
+    })
+      .then(function (response) {
+        coords.push(response.coord.lat);
+        coords.push(response.coord.lon);
+        let cityName = response.name;
+        let cityCond = response.weather[0].description.toUpperCase();
+        let cityTemp = response.main.temp;
+        let cityHum = response.main.humidity;
+        let cityWind = response.wind.speed;
+        let icon = response.weather[0].icon;
+        $("#icon").html(
+          `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
+        );
+        $("#city-name").html(cityName);
+        $("#city-cond").text("Current Conditions: " + cityCond);
+        $("#temp").text("Current Temp : " + cityTemp.toFixed(0));
+        $("#temp").append("°C");
+        $("#humidity").text("Humidity: " + cityHum + "%");
+        $("#wind-speed").text("Wind Speed: " + cityWind + "mph");
+        $("#date1").text(day1);
+        $("#date2").text(day2);
+        $("#date3").text(day3);
+        $("#date4").text(day4);
+        $("#date5").text(day5);
 
-      get5DayWeather(response.coord.lat, response.coord.lon);
-    }).fail(function (){
-      alert("Could not get data")
-    });
+        get5DayWeather(response.coord.lat, response.coord.lon);
+      })
+      .fail(function () {
+        alert("Please enter a valid city/town");
+      });
 
     //Function to get 5-day forecast
     function get5DayWeather(lat, lon) {
-     
-        
       $.ajax({
-        url: "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&appid=9f052728837492dee784a6ea4d36509f",
+        url:
+          "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&units=metric&appid=9f052728837492dee784a6ea4d36509f",
         method: "GET",
       }).then(function (response) {
-
-
-
         //5 day temp variables
         let day1temp = response.daily[1].temp.max;
         let day2temp = response.daily[2].temp.max;
@@ -137,11 +132,16 @@ $(document).ready(function () {
         let day4temp = response.daily[4].temp.max;
         let day5temp = response.daily[5].temp.max;
 
-        $("#temp1").text("Temp(F):" + " " + day1temp.toFixed(1));
-        $("#temp2").text("Temp(F):" + " " + day2temp.toFixed(1));
-        $("#temp3").text("Temp(F):" + " " + day3temp.toFixed(1));
-        $("#temp4").text("Temp(F):" + " " + day4temp.toFixed(1));
-        $("#temp5").text("Temp(F):" + " " + day5temp.toFixed(1));
+        $("#temp1").text("Temp:" + " " + day1temp.toFixed(0));
+        $("#temp1").append("°C");
+        $("#temp2").text("Temp:" + " " + day2temp.toFixed(0));
+        $("#temp2").append("°C");
+        $("#temp3").text("Temp:" + " " + day3temp.toFixed(0));
+        $("#temp3").append("°C");
+        $("#temp4").text("Temp:" + " " + day4temp.toFixed(0));
+        $("#temp4").append("°C");
+        $("#temp5").text("Temp:" + " " + day5temp.toFixed(0));
+        $("#temp5").append("°C");
 
         //5 day humidity variables
         let day1hum = response.daily[1].humidity;
@@ -162,7 +162,7 @@ $(document).ready(function () {
         let icon3 = response.daily[3].weather[0].icon;
         let icon4 = response.daily[4].weather[0].icon;
         let icon5 = response.daily[5].weather[0].icon;
-        
+
         $("#icon1").html(
           `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`
         );
@@ -181,7 +181,8 @@ $(document).ready(function () {
       });
     }
   }
-//function to add recently searched cities to page
+
+  //function to add recently searched cities to page
   function listCities() {
     $("#cityList").text("");
     cities.forEach((city) => {
@@ -190,14 +191,14 @@ $(document).ready(function () {
   }
 
   listCities();
-//event handler for recently searched cities in table
+  //event handler for recently searched cities in table
   $(document).on("click", "td", (e) => {
     e.preventDefault();
     let listedCity = $(e.target).text();
     city = listedCity;
     search();
   });
-//event handler for clear button
+  //event handler for clear button
   $("#clr-btn").click(() => {
     localStorage.removeItem("cities");
     loadRecentCities();
@@ -205,12 +206,9 @@ $(document).ready(function () {
     location.reload();
   });
 
-    // When clear button has been clicked load weather data for oswestry
-    if (cities.length === 0) {
-      city = "Oswestry";
-      search();
-    }
-  
-
+  // When clear button has been clicked load weather data for oswestry
+  if (cities.length === 0) {
+    city = "Oswestry";
+    search();
+  }
 });
-
